d3bbcb6a4cf9e092c0923ba8ccdf5b8ff8fb9c72
Embedthis Software <dev@embedthis.com>
2013-04-03 23:35:46 -0700

    Fix substitution of CFLAGS in make rules

--- projects/pcre-macosx-static.mk
+++ projects/pcre-macosx-static.mk
@@ -25,7 +25,7 @@ BIT_PACK_LINK_PATH        := link
 CFLAGS             += -w
 DFLAGS             +=  $(patsubst %,-D%,$(filter BIT_%,$(MAKEFLAGS))) 
 IFLAGS             += -I$(CONFIG)/inc
-LDFLAGS            += '-Wl,-rpath,@executable_path/' '-Wl,-rpath,@loader_path/' 
+LDFLAGS            += '-Wl,-rpath,@executable_path/' '-Wl,-rpath,@loader_path/'
 LIBPATHS           += -L$(CONFIG)/bin
 LIBS               += -ldl -lpthread -lm
 
